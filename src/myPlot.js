1d5ebc9e5d42c5a7e73f4d7e20d084850b6d57b6
mathew.brown <mathew.brown@hakai.org>
2023-02-02 09:52:27 -0800

    multiple plots

--- src/myPlot.js
+++ src/myPlot.js
@@ -1,63 +1,63 @@
 import { useRef, useEffect, useState, Component } from "react";
-import * as Plot from "@observablehq/plot";
-import { Dropdown, Selection } from 'react-dropdown-now';
+// import { Dropdown, Selection } from 'react-dropdown-now';
 import 'react-dropdown-now/style.css';
-import { Checkboxes } from "./checkboxes";
+import { CheckboxLabels } from "./checkboxes";
+import LineChart from "./lineChart.js"
 
-
+let ticker = 0
 
 function MyPlot({ data }) {
-    const allVars = ["ucur", "vcur", "uwnd", "vwnd", "hs", "lm", "t02", "fp", "dir", "dp", "wcc", "wch", "utwo", "vtwo", "foc", "uuss", "ß"]
-
-    const ref = useRef(); // create DOM node to chart
+    // console.log(data)
+    const allVars = ["ucur", "vcur", "uwnd", "vwnd", "hs", "lm", "t02", "fp", "dir", "dp", "wcc", "wch", "utwo", "vtwo", "foc", "uuss", "vuss"]
+    const units = ["eastward current, m s-1", "northward current, m s-1", "Eastward Wind,m s-1", "Northward Wind,m s-1", "significant height of wind waves,m", "mean wave length,m", "mean period T02,s", "wave peak frequency, s-1", "wave mean direction, degree", "peak direction, degree", "whitecap coverage, 1", "significant breaking wave height, m", "eastward wave to ocean stress, m2 s-2", "northward wave to ocean stress, m2 s-2", "wave to ocean energy flux, W m-2", "eastward surface stokes drift, m s-1", "northward surface stokes drift, m s-1"]
+    // const ref = useRef(); // create DOM node to chart
     const [selectedVars, setSelectedVars] = useState(allVars);
 
 
     const onCheckChange = (changedIndex) => {
-    const newSelectedVars = selectedVars.map((value, i) =>
-        i === changedIndex ? (selectedVars[i] ? false : allVars[i]) : value
-    );
-    setSelectedVars(newSelectedVars);
+
+        console.log(changedIndex, ticker)
+        const newSelectedVars = selectedVars.map((value, i) =>
+            i === changedIndex ? (selectedVars[i] ? false : allVars[i]) : value
+        );
+
+        if (ticker === 0) {
+            newSelectedVars[0] = false
+
+        }
+        ticker += 1
+        console.log(newSelectedVars)
+        setSelectedVars(newSelectedVars);
     };
 
-
-    const [optionValue, setOptionValue] = useState("wch");
-    const handleSelect = (e) => {
-        console.log(e)
-        console.log(e.value);
-        setOptionValue(e.value);
-    };
 
     useEffect(() => { //replace DOM contents with useEffect
 
-        const dotPlot = Plot.dot(
-            data.filter((d) => selectedVars.includes(d.wch)),
-            {
-                x: "time",
-                y: optionValue,
-                fill: 'steelblue',
-                fillOpacity: 0.5,
-                title: (d) =>
-                    [d.path, `${d.duration} ms`, `status ${d.status_code}`].join("\n")
-            }
-        ).plot({
-            marginTop: 50,
-            marginLeft: 50
-        });
-        ref.current.append(dotPlot);
-        return () => dotPlot.remove();
-    }, [data, optionValue]);
+        if (ticker === 0) { selectedVars[0] = false }
+        console.log(selectedVars, ticker)
+
+
+        console.log(selectedVars)
+    }, [data, selectedVars]);
 
     return (
         <div>
-          <Checkboxes
-            title="Status code"
-            onChange={onCheckChange}
-            options={allVars}
-          />
-          <div ref={ref}></div>
+            <CheckboxLabels
+                options={allVars}
+                onChange={onCheckChange}
+                units={units}
+            />
+            {selectedVars.map((items, i) =>
+            (selectedVars[i] !== allVars[i] ? <LineChart width={600} height={400} data={data.map(d => {
+                // console.log(units[i])
+                return { time: d.time, variable: d[allVars[i]] }
+            })} inputVar={allVars[i]}
+                unit={units[i]} /> : "")
+            )}
+
+
         </div>
-      );
+    );
 }
 
 
