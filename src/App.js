b8d18d40b8435380791845a7f5dc103c1928fcdb
mathew.brown <mathew.brown@hakai.org>
2023-02-14 13:54:54 -0800

    setup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import "./styles.css";
 import MyPlot from "./myPlot"
-import * as d3 from "d3";
 import { useRef, useEffect, useState } from "react";
 
 
 function App() {
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const offset = new Date().getTimezoneOffset();
-
-    d3.csv("/ubcSSf2DWaveFields30m.csv", d3.autoType).then(dataRaw => {
-      return dataRaw.map(d => {
-        d.date = new Date(d.time.getTime() - offset*60*1000)
-      return d})
-
-    }).then(dataout => {
-      console.log(dataout)
-      setData(dataout);
-      setLoading(false);
-    });
-  }, []);
 
   return (
     <div className="App">
-      <h1>Plot in React</h1>
-      {loading && <div>loading</div>}
-      {!loading && <MyPlot
-        data={data}
+      <h1>plot ss data</h1>
+      {/* {loading && <div>loading</div>} */}
+      {<MyPlot
       />}
     </div>
   );
--- src/myPlot.js
+++ src/myPlot.js
@@ -1,59 +1,79 @@
 import { useRef, useEffect, useState, Component } from "react";
 // import { Dropdown, Selection } from 'react-dropdown-now';
 import 'react-dropdown-now/style.css';
-import { CheckboxLabels } from "./checkboxes";
+import { RadioButtonsGroup } from "./radiobutts";
 import LineChart from "./lineChart.js"
+// import Slider from "./slider.js"
+import * as d3 from "d3";
 
-let ticker = 0
+// let ticker = 0
 
-function MyPlot({ data }) {
-    // console.log(data)
-    const allVars = ["ucur", "vcur", "uwnd", "vwnd", "hs", "lm", "t02", "fp", "dir", "dp", "wcc", "wch", "utwo", "vtwo", "foc", "uuss", "vuss"]
-    const units = ["eastward current, m s-1", "northward current, m s-1", "Eastward Wind,m s-1", "Northward Wind,m s-1", "significant height of wind waves,m", "mean wave length,m", "mean period T02,s", "wave peak frequency, s-1", "wave mean direction, degree", "peak direction, degree", "whitecap coverage, 1", "significant breaking wave height, m", "eastward wave to ocean stress, m2 s-2", "northward wave to ocean stress, m2 s-2", "wave to ocean energy flux, W m-2", "eastward surface stokes drift, m s-1", "northward surface stokes drift, m s-1"]
+function MyPlot({field }) {
+
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      const offset = new Date().getTimezoneOffset();
+  
+      d3.csv("/ubcSSf2DWaveFields30m.csv", d3.autoType).then(dataRaw => {
+        return dataRaw.map(d => {
+          d.date = new Date(d.time.getTime() - offset*60*1000)
+        return d})
+  
+      }).then(dataout => {
+        console.log(dataout)
+        setData(dataout);
+        setLoading(false);
+      });
+    }, []);
+
+
+    
+
+    const allVars = ["potentialTemperature", "salinity", "seaSurfaceHeight", "airTemperature", "current", "wind"]
+    const units = ["°C", "stuff","m", "°C", "m-2 s-1", "m-2 s-1"]
     // const ref = useRef(); // create DOM node to chart
-    const [selectedVars, setSelectedVars] = useState(allVars);
+    const [selectedVar, setSelectedVar] = useState(allVars);
+    const [selectedUnit, setSelectedUnit] = useState(allVars);
 
 
     const onCheckChange = (changedIndex) => {
 
-        console.log(changedIndex, ticker)
-        const newSelectedVars = selectedVars.map((value, i) =>
-            i === changedIndex ? (selectedVars[i] ? false : allVars[i]) : value
-        );
+        console.log(allVars[changedIndex])
+        // const newSelectedVars = selectedVars.map((value, i) =>
+        //     i === changedIndex ? (selectedVars[i] ? false : allVars[i]) : value
+        // );
 
-        if (ticker === 0) {
-            newSelectedVars[0] = false
+        // if (ticker === 0) {
+        //     newSelectedVars[0] = false
 
-        }
-        ticker += 1
-        console.log(newSelectedVars)
-        setSelectedVars(newSelectedVars);
+        // }
+        // ticker += 1
+        setSelectedVar(allVars[changedIndex]);
+        setSelectedUnit(units[changedIndex]);
     };
 
 
     useEffect(() => { //replace DOM contents with useEffect
 
-        if (ticker === 0) { selectedVars[0] = false }
-        console.log(selectedVars, ticker)
 
-
-        console.log(selectedVars)
-    }, [data, selectedVars]);
+    }, [data, selectedVar]);
 
     return (
         <div>
-            <CheckboxLabels
+            <RadioButtonsGroup
                 options={allVars}
                 onChange={onCheckChange}
                 units={units}
             />
-            {selectedVars.map((items, i) =>
-            (selectedVars[i] !== allVars[i] ? <LineChart width={600} height={400} data={data.map(d => {
-                // console.log(units[i])
-                return { time: d.time, variable: d[allVars[i]] }
-            })} inputVar={allVars[i]}
-                unit={units[i]} /> : "")
-            )}
+            {/* <Slider/> */}
+            (
+            <LineChart width={600} height={400} data={data.map(d => {
+                return { time: d.time, variable: selectedVar }
+            })} inputVar={selectedVar}
+                unit={selectedUnit} /> )
+            {/* )} */}
 
 
         </div>
